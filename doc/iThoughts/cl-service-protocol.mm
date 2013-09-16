1eaaadc5ddbc4590ea0ae1301d684567a1deb86b
Sean Champ <spchamp+git-muletop@me.com>
2013-09-16 10:49:26 -0700

    adding notes (iThoughts)

---
+++ doc/iThoughts/cl-service-protocol.mm
@@ -0,0 +1,103 @@
+<map version="0.9.0"><attribute_registry SHOW_ATTRIBUTES="hide"/>
+<node TEXT="Towards an Abstract Service Protocol for Common Lisp Applications and a Complimentary Extension into IP Networking and Two-Wire Device Protocols&#10;&#10;Was: Trivial-Networking&#10;&#10;Notes&#10;&#10;Sean Champ&#10;11 Sep 2013" ID="Freemind_Link_1"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Note: Special role of AF_UNSPEC socket addressing in datagram-oriented networking (UDP)" ID="Freemind_Link_94"  POSITION="right"  COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><attribute NAME="Floating" VALUE="{1954, -292}"/><node TEXT="Networking/NameResolution - FedoraProject" ID="Freemind_Link_95" LINK="https://fedoraproject.org/wiki/Networking/NameResolution"  COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="Safari | Linux Socket Programming by Example -&gt; The AF_UNSPEC Address Family" ID="Freemind_Link_96" LINK="http://alas.matf.bg.ac.rs/manuals/lspe/snode=30.html"  COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Safari | Linux Socket Programming by Example" ID="Freemind_Link_97" LINK="http://alas.matf.bg.ac.rs/manuals/lspe/mode=1.html"  COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+</node>
+<node TEXT="Conventional Networking Architecture &#10;as Defining a Networking Reference Model" ID="Freemind_Link_2"  POSITION="right"  COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Reference Materials" ID="Freemind_Link_66"   COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><cloud/><edge COLOR="#FFD9B2" /><attribute NAME="Callout" VALUE="{-296, -190}"/><node TEXT="Sockets - The GNU C Library" ID="Freemind_Link_67" LINK="http://www.gnu.org/software/libc/manual/html_node/Sockets.html#Sockets"  COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /></node>
+<node TEXT="Beej's Guide to Network Programming" ID="Freemind_Link_68" LINK="http://beej.us/guide/bgnet/output/html/multipage/index.html"  COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /></node>
+<node TEXT="Linux Socket Programming by Example - Warren Gay" ID="Freemind_Link_69" LINK="http://os.reading-now.org/book/41"  COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /></node>
+</node>
+<node TEXT="IP Networking" ID="Freemind_Link_3"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="IP Address Space" ID="Freemind_Link_4"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="IP Address Formats" ID="Freemind_Link_5"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="IPv4" ID="Freemind_Link_6"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="IPv6" ID="Freemind_Link_7"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+<node TEXT="Concepts" ID="Freemind_Link_8"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Network interface" ID="Freemind_Link_9"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="e.g eth0, wlan0, ppp0" ID="Freemind_Link_10"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+<node TEXT="Network host" ID="Freemind_Link_11"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Host address" ID="Freemind_Link_12"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="Host name" ID="Freemind_Link_13"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+<node TEXT="Routing" ID="Freemind_Link_14"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Gateway Address" ID="Freemind_Link_15"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="Netmask" ID="Freemind_Link_16"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+<node TEXT="Reserved addresses" ID="Freemind_Link_17"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="cf. Reserved IP addresses - Wikipedia, the free encyclopedia" ID="Freemind_Link_18" LINK="http://en.wikipedia.org/wiki/Reserved_IP_addresses"  COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+</node>
+</node>
+<node TEXT="IP Protocol Types" ID="Freemind_Link_19"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="TCP" ID="Freemind_Link_20"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Connection-Oriented" ID="Freemind_Link_21"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+<node TEXT="UDP" ID="Freemind_Link_22"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Connectionless" ID="Freemind_Link_23"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+</node>
+<node TEXT="Application Protocols" ID="Freemind_Link_24"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><icon BUILTIN="go"/>
+<node TEXT="HTTP" ID="Freemind_Link_25"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="SSH" ID="Freemind_Link_26"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="FTP" ID="Freemind_Link_27"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="IIOP" ID="Freemind_Link_28"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="&#x2026;" ID="Freemind_Link_29"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+<node TEXT="Operating System View of Networking" ID="Freemind_Link_30"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="GNU LibC Sockets Model" ID="Freemind_Link_31"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><cloud/><edge COLOR="#B2B2FE" /><node TEXT="Socket Types" ID="Freemind_Link_32"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Stream Sockets&#10;(SOCK_STREAM)" ID="Freemind_Link_33"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Suitable for TCP application protocols" ID="Freemind_Link_34"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="tcp(7) - Linux manual page" ID="Freemind_Link_35" LINK="http://man7.org/linux/man-pages/man7/tcp.7.html"  COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+<node TEXT="Datagram Sockets&#10;(SOCK_DGRAM)" ID="Freemind_Link_36"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Suitable for UDP application protocols" ID="Freemind_Link_37"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="udp(7) - Linux manual page" ID="Freemind_Link_38" LINK="http://man7.org/linux/man-pages/man7/udp.7.html"  COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+<node TEXT="Raw Sockets&#10;(SOCK_RAW)" ID="Freemind_Link_39"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Suitable for lower-level networking protocols (e.g ICMP, ARP)" ID="Freemind_Link_40"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="icmp(7) - Linux manual page" ID="Freemind_Link_41" LINK="http://man7.org/linux/man-pages/man7/icmp.7.html"  COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="arp(7) - Linux manual page" ID="Freemind_Link_42" LINK="http://man7.org/linux/man-pages/man7/arp.7.html"  COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+<node TEXT="raw(7) - Linux manual page" ID="Freemind_Link_43" LINK="http://man7.org/linux/man-pages/man7/raw.7.html"  COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+</node>
+<node TEXT="Socket Address Formats" ID="Freemind_Link_44"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><arrowlink COLOR="#477DB2" DESTINATION="Freemind_Link_4" ENDARROW="Default" ENDINCLINATION="22;0;" ID="Arrow_ID_104" STARTARROW="None" STARTINCLINATION="22;0;" SOURCE_LABEL="" MIDDLE_LABEL="Implements" TARGET_LABEL="" /><node TEXT="e.g" ID="Freemind_Link_45"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><arrowlink COLOR="#477DB2" DESTINATION="Freemind_Link_52" ENDARROW="Default" ENDINCLINATION="22;0;" ID="Arrow_ID_105" STARTARROW="None" STARTINCLINATION="22;0;" SOURCE_LABEL="" MIDDLE_LABEL="cf." TARGET_LABEL="" /><node TEXT="AF_UNIX" ID="Freemind_Link_46"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Synonymous with AF_FILE" ID="Freemind_Link_47"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="Beyond the scope of IP networking" ID="Freemind_Link_48"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+<node TEXT="AF_INET" ID="Freemind_Link_49"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="AF_INET6" ID="Freemind_Link_50"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+</node>
+<node TEXT="Namespaces and Protocol Families" ID="Freemind_Link_51"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="e.g" ID="Freemind_Link_52"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Filesystem Namespace" ID="Freemind_Link_53"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="PF_UNIX" ID="Freemind_Link_54"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Synonymous with PF_FILE" ID="Freemind_Link_55"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="Beyond the scope of IP networking" ID="Freemind_Link_56"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+</node>
+<node TEXT="IP Namespace" ID="Freemind_Link_57"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="PF_INET" ID="Freemind_Link_58"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="PF_INET6" ID="Freemind_Link_59"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="ICMP" ID="Freemind_Link_60"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="May be implemented via raw sockets&#10;&#10;cf. article at link" ID="Freemind_Link_61" LINK="http://bit.ly/15Pg9sw"  COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+</node>
+<node TEXT="Illustrations" ID="Freemind_Link_62"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Trivial Architectural Model" ID="Freemind_Link_63"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><font BOLD="false" ITALIC="true" SIZE="14"/></node>
+<node TEXT="Trivial Socket Program Control Flow" ID="Freemind_Link_64"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><font BOLD="false" ITALIC="true" SIZE="14"/><node TEXT="Not conveyed clearly in this thumbnail" ID="Freemind_Link_65"   COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /><attribute NAME="Callout" VALUE="{367, -2}"/></node>
+</node>
+</node>
+</node>
+<node TEXT="Note: Interface Naming - The GNU C Library" ID="Freemind_Link_98" LINK="http://www.gnu.org/software/libc/manual/html_node/Interface-Naming.html#Interface-Naming" POSITION="right"  COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><font BOLD="false" ITALIC="false" SIZE="14"/><attribute NAME="Floating" VALUE="{541, 254}"/></node>
+<node TEXT="To Do: Sync project to GitHub" ID="Freemind_Link_101"  POSITION="left"  COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /><attribute NAME="Callout" VALUE="{-191, 269}"/></node>
+<node TEXT="API Sketch" ID="Freemind_Link_70"  POSITION="right"  COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /><node TEXT="Create-Server-Peer" ID="Freemind_Link_71"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="Create-Client-Peer" ID="Freemind_Link_72"   COLOR="#000000" BACKGROUND_COLOR="#B2B2FE" STYLE="bubble" ><edge COLOR="#B2B2FE" /></node>
+<node TEXT="The &quot;Create Peer&quot; operations can be specialized on one type of resource.&#10;&#10;What type of resource should the operations be specialized on?" ID="Freemind_Link_73"   COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /><attribute NAME="Callout" VALUE="{179, 158}"/><node TEXT="A class, NETWORK-SERVICE, may be defined, such as to serve as a base class for application layer protocols such as HTTP, SSH" ID="Freemind_Link_74"   COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /><node TEXT="The class may be abstracted as to address other contractual service protocols, such as for device connections in the hardware layer" ID="Freemind_Link_75"   COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /><node TEXT="Superclass: SERVICE, instances of which would represent the implementation of a service protocol, in a protocol-specific regard" ID="Freemind_Link_77"   COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /><attribute NAME="Callout" VALUE="{10, -110}"/><node TEXT="Try to follow on this with generic function definitions suitable to the definition of an abstract service protocol, and its extension into IP networking, HTTP and IIOP specifically, and a device-oriented protocol suitable to application integration (I2C?)" ID="Freemind_Link_78"   COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /><node TEXT="I2C and SMBus" ID="Freemind_Link_79"   COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /><icon BUILTIN="itmz-hand-thumb-up"/>
+<node TEXT="Overview" ID="Freemind_Link_80"   COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /><node TEXT="I&#xb2;C - Wikipedia, the free encyclopedia" ID="Freemind_Link_81" LINK="http://en.wikipedia.org/wiki/I&#xb2;C"  COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /></node>
+<node TEXT="System Management Bus - Wikipedia, the free encyclopedia" ID="Freemind_Link_82" LINK="http://en.wikipedia.org/wiki/SMBus"  COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /></node>
+</node>
+<node TEXT="Applications" ID="Freemind_Link_83"   COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /><node TEXT="I2C Tutorial" ID="Freemind_Link_84" LINK="http://www.robot-electronics.co.uk/acatalog/I2C_Tutorial.html"  COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /></node>
+<node TEXT="avr-libc: Example using the two-wire interface (TWI)" ID="Freemind_Link_85" LINK="https://ccrma.stanford.edu/courses/250a/docs/avr-libc-user-manual-1.2.5/group__twi__demo.html"  COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /></node>
+</node>
+<node TEXT="Devices" ID="Freemind_Link_86"   COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /><node TEXT="Simple Devices" ID="Freemind_Link_87"   COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /><node TEXT="LEDs" ID="Freemind_Link_88"   COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /><node TEXT="thingm :: an electronic product studio: BlinkM" ID="Freemind_Link_89" LINK="http://thingm.com/products/blinkm"  COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /></node>
+</node>
+<node TEXT="I2C to USB Adapters" ID="Freemind_Link_90"   COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /><node TEXT="Aardvark I2C/SPI Host Adapter - USB to I2C and SPI interface" ID="Freemind_Link_91" LINK="http://www.totalphase.com/products/aardvark_i2cspi/"  COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /></node>
+<node TEXT="I2c-tiny-USB" ID="Freemind_Link_92" LINK="http://www.harbaum.org/till/i2c_tiny_usb/index.shtml"  COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /></node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Note that IP networking is, itself, comprised of a stack or semantic network of related protocols, from ARP (Ethernet MAC addressing) on to IP addressing, DNS name query services (recently augmented with Bonjour and Zeroconf service protocols) and the application layer protocols such as HTTP, IIOP, SSH, Mosh, DNS queries, etc" ID="Freemind_Link_76"   COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /></node>
+<node TEXT="Must be suitable to connectionless (e.g DNS requests over UDP) and connection-oriented protocols (e.g HTTP, IIOP)" ID="Freemind_Link_93"   COLOR="#000000" BACKGROUND_COLOR="#FFD9B2" STYLE="bubble" ><edge COLOR="#FFD9B2" /><font BOLD="false" ITALIC="false" SIZE="14"/><attribute NAME="Callout" VALUE="{-65, 151}"/></node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
